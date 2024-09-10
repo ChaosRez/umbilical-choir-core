485855ed363a8d2e61a0a5c6505ffd84ce621012
Reza <r.malek@keemail.me>
2024-09-10 17:01:40 +0200

    add a parent address

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"encoding/json"
+	"errors"
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/geojson"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
 	"os"
@@ -17,12 +21,18 @@ type Config struct {
 		Location  string `yaml:"location,omitempty"`
 	} `yaml:"faas"`
 	Agent struct {
-		Host string `yaml:"host"`
+		Host        string `yaml:"host"`
+		ServiceArea string `yaml:"service_area"`
 	} `yaml:"agent"`
+	Parent struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	} `yaml:"parent"`
 	LogLevel string `yaml:"logLevel"`
 }
 
 func LoadConfig(path string) (*Config, error) {
+	log.Infof("Loading config from %s", path)
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -38,6 +48,7 @@ func LoadConfig(path string) (*Config, error) {
 	return &config, nil
 }
 
+// Set logger's level (from config) and format
 func InitLogger(logLevel string) {
 	ll, err := log.ParseLevel(logLevel)
 	if err != nil {
@@ -45,4 +56,25 @@ func InitLogger(logLevel string) {
 	}
 	log.SetLevel(ll)
 	log.SetFormatter(&log.TextFormatter{TimestampFormat: "15:04:05.000", FullTimestamp: true})
+}
+
+// Helper fuction to unmarshall the service area polygon
+func (cfg *Config) StrAreaToPolygon() (orb.Polygon, error) {
+	log.Debug("Parsing service area from config")
+	var fc geojson.FeatureCollection
+	err := json.Unmarshal([]byte(cfg.Agent.ServiceArea), &fc)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(fc.Features) == 0 {
+		return nil, errors.New("Service area is empty or invalid")
+	}
+
+	polygon, ok := fc.Features[0].Geometry.(orb.Polygon)
+	if !ok {
+		return nil, errors.New("Service area is not a valid polygon")
+	}
+
+	return polygon, nil
 }
